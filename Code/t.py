ebfc17a926da46c1de8041a3e21eaadf30af58f8
dong73097@gmail.com <dong73097@gmail.com>
2024-07-15 21:31:39 +0900

    m

---
+++ Code/t.py
@@ -0,0 +1,16 @@
+import private_data
+from supabase import create_client, Client
+from openai import OpenAI
+Client = create_client(private_data.dburl, private_data.dbkey)
+
+
+oclient = OpenAI(api_key=private_data.aikey)
+
+def get_embedding_list(texts, model="text-embedding-3-large"):
+   return oclient.embeddings.create(input = texts, model=model).data[0].embedding
+
+topic = "생일"
+emb = get_embedding_list(topic)
+print(Client.table("category").insert({"title": topic, "embedding": emb}).execute())
+
+print(Client.table("category").select("*").execute().data[0])
